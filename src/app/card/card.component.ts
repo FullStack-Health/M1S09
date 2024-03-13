f1bb5d27d89ffd5002057cd5fe551dc3c9fbecb8
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-13 17:29:19 -0300

    Atividade Lista de Produtos - partes 1 e 2

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  produto = 'Banana';
+  valor = '2,20'
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
