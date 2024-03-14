0befccd7fa5b7bfba8d730dd9168308369356f50
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-13 22:05:03 -0300

    Atividade Lista de Produtos - parte 3

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  listaProdutos = [
+    {
+      imagem: 'assets/produto.png',
+      descricao: 'Banana'
+    },
+    {
+      imagem: 'assets/produto.png',
+      descricao: 'Maça'
+    },
+    {
+      imagem: 'assets/produto.png',
+      descricao: 'Pera'
+    },
+    {
+      imagem: 'assets/produto.png',
+      descricao: 'Uva'
+    }
+  ];
 }
